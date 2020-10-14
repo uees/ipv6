3973410eca12b3670d6a37c5a3c2f77dd2ebd616
Wan <wnh3yang@gmail.com>
2020-10-14 22:46:27 +0800

    lcoal api

--- ipv6.go
+++ ipv6.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"main/localip"
 	"net/http"
 
 	"github.com/xjh22222228/ip"
@@ -18,8 +19,22 @@ func getIPV6(writer http.ResponseWriter, request *http.Request) {
 	}
 }
 
+// getIPV6Local 利用本地 API
+func getIPV6Local(writer http.ResponseWriter, request *http.Request) {
+	request.ParseForm()
+	ipv6Type := request.Form.Get("type")
+	ipv6 := localip.Ipv6(ipv6Type)
+
+	if ipv6 == "" {
+		fmt.Fprintf(writer, "ERROR: not get ipv6")
+	} else {
+		fmt.Fprintf(writer, ipv6)
+	}
+}
+
 func main() {
 	http.HandleFunc("/", getIPV6)
+	http.HandleFunc("/localip", getIPV6Local)
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
---
+++ localip/core.go
@@ -0,0 +1,41 @@
+package localip
+
+import (
+	"log"
+	"os/exec"
+	"regexp"
+)
+
+// Ipv6 返回本地分配的 ipv6
+func Ipv6(ipv6Type string) string {
+	var reMatch string
+	if ipv6Type == "fixed" {
+		reMatch = `inet6 ([^f:][\da-f:]+)/\d+ scope global.?\n.+?valid_lft forever preferred_lft forever`
+	} else {
+		reMatch = `inet6 ([^f:][\da-f:]+)/\d+ scope global mngtmpaddr dynamic`
+	}
+
+	command := "ip addr show|grep -v deprecated|grep -A1 'inet6 [^f:]'"
+	cmd := exec.Command("/bin/bash", "-c", command)
+	bytes, err := cmd.Output()
+	if err != nil {
+		log.Println("command err:", err)
+		return ""
+	}
+	resp := string(bytes)
+
+	re := regexp.MustCompile(reMatch)
+	if re == nil {
+		log.Println("regexp err")
+		return ""
+	}
+
+	result := re.FindStringSubmatch(resp)
+
+	if result == nil || len(result) == 0 {
+		log.Println("match err")
+		return ""
+	}
+
+	return result[1]
+}
